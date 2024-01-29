d49a186849f8db67f811fc6fc312b1c563b3e19a
soytmen <soytmen@gmail.com>
2024-01-29 13:44:12 +0100

    asdsadasdadasdsa

--- Assets/Scripts/MoveCamera.cs
+++ Assets/Scripts/MoveCamera.cs
@@ -1,12 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
-{
-    public Transform cameraPosition;
-    private void Update()
-    {
-        transform.position = cameraPosition.position;
+public class MoveCamera : MonoBehaviour {
+
+    public Transform player;
+
+    void Update() {
+        transform.position = player.transform.position;
     }
 }
